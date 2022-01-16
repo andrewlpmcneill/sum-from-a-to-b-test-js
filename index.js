5daa7b10eca2dc63e9684c89e80723f9159e133b
Andrew McNeill <andrew@andrewmc.ca>
2022-01-16 17:10:20 -0400

    Built out functionality as described

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+const sum = (fromN, toN) => {
+
+  if (toN === fromN) {
+    return fromN;
+  }
+
+  return toN + (sum(fromN, toN - 1));
+
+};
 
 module.exports = sum;
+
+console.log(sum(1, 10));
